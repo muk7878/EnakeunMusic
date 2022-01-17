eb3484489ce01e459a84ec6d67e6be8d9fec2d59
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-17 20:29:41 +0700

    Update callback_handlers.py

--- plugins/callback_handlers.py
+++ plugins/callback_handlers.py
@@ -111,7 +111,7 @@ async def goback(client: Client, hee: CallbackQuery):
 async def cbhelp(_, lol: CallbackQuery):
     match = lol.matches[0].group(1)
     chat_id = lol.message.chat.id
-    user_id = int(lol.matches[0].group(3))
+    user_id = int(lol.matches[0].group(2))
     if match == "cbhelp":
         return await lol.edit_message_text(
             gm(chat_id, "helpmusic"),
